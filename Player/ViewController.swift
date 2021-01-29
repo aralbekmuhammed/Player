bd81391c5ff1f713238b052b5a4f0a7b31ac58de
Aralbek Muhammed <a_muhammed@icloud.com>
2021-01-29 21:47:04 +0600

    Initial Commit

---
+++ Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Player
+//
+//  Created by Мухаммед Аралбек on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
