36e26e817c0916737139df6e7723f3ca7f2d1772
Aralbek Muhammed <a_muhammed@icloud.com>
2021-01-29 22:01:56 +0600

    update

--- Player/ViewController.swift
+++ Player/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
-
+import MediaPlayer
 class ViewController: UIViewController {
-
+    @objc var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @objc func playGenre(genre: String) {
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
     }
 
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        
+        MPMediaLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                DispatchQueue.main.async {
+                    self.playGenre(genre: sender.currentTitle!)
+                    
+                }
+            }else {
+                self.musicPlayer.stop()
+            }
+        }
+    }
+    
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
 
 }
 
